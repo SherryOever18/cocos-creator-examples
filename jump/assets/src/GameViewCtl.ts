066e75b7c62282b67f8de75617107f7a6ceb7f72
lamyoung <273404202@qq.com>
2022-06-02 07:46:43 +0800

    jump step3 ui prefab

--- jump/assets/src/GameViewCtl.ts
+++ jump/assets/src/GameViewCtl.ts
@@ -1,12 +1,19 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameViewCtl')
 export class GameViewCtl extends Component {
+    @property(Prefab)
+    resultViewPrefab: Prefab = null!
+
     @property(Node)
+    gameView: Node = null!
+
     resultView: Node = null!
 
     start() {
+        this.resultView = instantiate(this.resultViewPrefab)
+        this.gameView.addChild(this.resultView)
         this.resultView.active = false;
     }
 
